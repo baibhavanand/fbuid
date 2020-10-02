53cc0167191789e34131b1444f1a0bc6681725d5
Baibhav Anand <35161513+baibhavanand@users.noreply.github.com>
2020-10-02 19:26:42 +0545

    Uploaded Installation file

---
+++ install.sh
@@ -0,0 +1,9 @@
+
+#!/usr/bin/env bash
+# Authors: Baibhav Anand Jha
+# Documentation: github.com/baibhavanand/Fbuid
+
+sudo chmod 777 fbuid
+sudo mv fbuid /usr/local/bin
+
+echo "Installtion Successful"
